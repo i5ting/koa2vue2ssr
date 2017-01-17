e97e0049cbaca98763aa3b3fe1e114eafb679dfc
i5ting <i5ting@126.com>
2017-01-18 05:53:23 +0800

    lazycommmit, please ignore

---
+++ src/components/Topics.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div v-for="topic in topics">
+      <p>{{topic.title}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+const fetchInitialData = store => {
+  return store.dispatch(`getTopics`)
+}
+export default {
+  prefetch: fetchInitialData,
+  computed: {
+    ...mapGetters({
+      topics: 'getTopics'
+    })
+  },
+  mounted () {
+    fetchInitialData(this.$store)
+  }
+}
+</script>
